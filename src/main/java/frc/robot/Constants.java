1fb2486d0c30e8f1fe382d8cde9595cc9a88bbb0
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2024-08-20 20:40:16 -0400

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosly.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int JoystickPort = 0;
+    public static final int JoystickTranslationAxis = 1;
+    public static final int JoystickStrafeAxis = 0;
+    public static final int JoystickRotationAxis = 2;
+    public static final int JoystickSliderAxis = 3;
+    public static final int JoystickRobotRelative = 12;
+  }
+
+  public static class SwerveConstants {
+
+    // flipped because originally there was too much slipping
+    public static final double trackWidth = Units.inchesToMeters(24.5);
+    public static final double wheelBase = Units.inchesToMeters(18.5);
+
+    // nominal (real) divided by fudge factor
+    public static final double wheelDiameter = Units.inchesToMeters(4.0 / 1.04085);
+    public static final double wheelCircumference = 2.0 * wheelDiameter * Math.PI;
+
+    // Kinematics gets each module relative to center. X is forward/backward and Y is left/right, left is positive
+    public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+      new Translation2d(trackWidth / 2.0, wheelBase / 2.0), // front left 
+      new Translation2d(trackWidth / 2.0, -wheelBase / 2.0), // front right
+      new Translation2d(-trackWidth / 2.0, wheelBase / 2.0), // back left
+      new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0) // back right
+    );
+
+    public static final double kPTurning = 0.25;
+
+    public static final double driveGearRatio = (6.75 / 1.0); // 6.75:1
+    public static final double rotationGearRatio = (150.0 / 7.0);
+
+    public static final double driveBaseRadius = Units.inchesToMeters(18.5);
+
+    public static final double maxMetersPerSecond = 2;
+
+    // front left
+    public static final int frontLeftDriveMotorId = 7;
+    public static final int frontLeftRotationMotorId = 8;
+    public static final int frontLeftCanCoderId = 12;
+    public static final double frontLeftOffsetRad = 0.287842 * 2 * Math.PI;
+
+    // front right
+    public static final int frontRightDriveMotorId = 5;
+    public static final int frontRightRotationMotorId = 6;
+    public static final int frontRightCanCoderId = 11;
+    public static final double frontRightOffsetRad = 0.617676 * 2 * Math.PI;
+
+    // back left
+    public static final int backLeftDriveMotorId = 3;
+    public static final int backLeftRotationMotorId = 4;
+    public static final int backLeftCanCoderId = 13;
+    public static final double backLeftOffsetRad = 0.899658 * 2 * Math.PI;
+
+    // back right
+    public static final int backRightDriveMotorId = 1;
+    public static final int backRightRotationMotorId = 2;
+    public static final int backRightCanCoderId = 14;
+    public static final double backRightOffsetRad = 0.496826 * 2 * Math.PI;
+
+    public static final double kTeleDriveMaxAcceleration = 15;
+    public static final double kTeleDriveMaxAngularAcceleration = 5;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,228 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.commands.SwerveJoystickCommand;
+
+public class SwerveSubsystem extends SubsystemBase {
+  /** Creates a new SwerveSubsystem. */
+  private final SwerveModule frontLeft = new SwerveModule(
+    SwerveConstants.frontLeftDriveMotorId, 
+    SwerveConstants.frontLeftRotationMotorId, 
+    SwerveConstants.frontLeftCanCoderId, 
+    SwerveConstants.frontLeftOffsetRad);
+
+  private final SwerveModule frontRight = new SwerveModule(
+    SwerveConstants.frontRightDriveMotorId, 
+    SwerveConstants.frontRightRotationMotorId, 
+    SwerveConstants.frontRightCanCoderId, 
+    SwerveConstants.frontRightOffsetRad);
+
+  private final SwerveModule backLeft = new SwerveModule(
+    SwerveConstants.backLeftDriveMotorId, 
+    SwerveConstants.backLeftRotationMotorId, 
+    SwerveConstants.backLeftCanCoderId, 
+    SwerveConstants.backLeftOffsetRad);
+
+  private final SwerveModule backRight = new SwerveModule(
+    SwerveConstants.backRightDriveMotorId, 
+    SwerveConstants.backRightRotationMotorId, 
+    SwerveConstants.backRightCanCoderId, 
+    SwerveConstants.backRightOffsetRad);
+
+  private final AHRS navX;
+
+  private final SwerveDriveOdometry odometry = new SwerveDriveOdometry(Constants.SwerveConstants.kinematics, new Rotation2d(), getModulePositions());
+
+  public SwerveSubsystem() {
+    navX = new AHRS(SPI.Port.kMXP);
+
+    new Thread(() -> {
+      try {
+        Thread.sleep(1000);
+        navX.reset();
+        odometry.resetPosition(new Rotation2d(), getModulePositions(), new Pose2d());
+      } catch (Exception e) {
+      }
+    }).start();
+
+    // initialize CANcoder offsets
+    frontLeft.initRotationOffset();
+    frontRight.initRotationOffset();
+    backLeft.initRotationOffset();
+    backRight.initRotationOffset();
+
+    // inversions -- need to figure these out
+    frontLeft.getDriveMotor().setInverted(true);
+    frontRight.getDriveMotor().setInverted(false);
+    backLeft.getDriveMotor().setInverted(false);
+    backRight.getDriveMotor().setInverted(false);
+
+    // reset encoders upon each start
+    frontLeft.resetEncoders();
+    frontRight.resetEncoders();
+    backLeft.resetEncoders();
+    backRight.resetEncoders();
+
+    AutoBuilder.configureHolonomic(
+      this::getPose, // robot pose supplier
+      this::resetOdometry, // method to reset odometry (will be called if auto has a starting pose)
+      this::getRobotRelativeSpeeds, // ChassisSpeeds supplier (must be robot relative)
+      this::driveRobotRelative, // method that will drive the robot given robot relative ChassisSpeeds
+      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+        new PIDConstants(1, 0, 0), // Translation PID constants
+        new PIDConstants(0, 0, 0), // Rotation PID constants
+        SwerveConstants.maxMetersPerSecond, // Max module speed in m/s
+        SwerveConstants.driveBaseRadius, // drive base radius in meters, distance from robot center to furthest module
+        new ReplanningConfig()// Default path replanning config, see the API for options
+      ),
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this
+    );
+  }
+
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    odometry.resetPosition(getHeading(), getModulePositions(), pose);
+  }
+
+  public ChassisSpeeds getRobotRelativeSpeeds() {
+    ChassisSpeeds chassisSpeeds = SwerveConstants.kinematics.toChassisSpeeds(getStates());
+
+    return chassisSpeeds;
+  }
+
+  public void zeroHeading() {
+    navX.reset();
+  }
+
+  public Rotation2d getHeading() {
+    // still not sure why I had to add the +90... needed to properly orient
+      // when field-relative
+    return Rotation2d.fromDegrees(-navX.getYaw() + 90);
+  }
+
+  public SwerveModulePosition[] getModulePositions() {
+    SwerveModulePosition[] positions = {
+      new SwerveModulePosition(-frontLeft.getCurrentDistanceMetersPerSecond(), frontLeft.getIntegratedAngle()),
+      new SwerveModulePosition(-frontRight.getCurrentDistanceMetersPerSecond(), frontRight.getIntegratedAngle()),
+      new SwerveModulePosition(-backLeft.getCurrentDistanceMetersPerSecond(), backLeft.getIntegratedAngle()),
+      new SwerveModulePosition(-backRight.getCurrentDistanceMetersPerSecond(), backRight.getIntegratedAngle())
+    };
+
+    return positions;
+  }
+
+  public void driveRobotRelative(ChassisSpeeds chassisSpeeds) {
+    ChassisSpeeds discreteSpeeds = ChassisSpeeds.discretize(chassisSpeeds, 0.02);
+    SwerveModuleState[] newStates = Constants.SwerveConstants.kinematics.toSwerveModuleStates(discreteSpeeds);
+    SwerveDriveKinematics.desaturateWheelSpeeds(newStates, Constants.SwerveConstants.maxMetersPerSecond);
+    setModuleStates(newStates);
+  }
+
+  public void driveFieldRelative(ChassisSpeeds chassisSpeeds) {
+    ChassisSpeeds discreteSpeeds = ChassisSpeeds.discretize(chassisSpeeds, 0.02);
+    discreteSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(discreteSpeeds, getHeading());
+    SwerveModuleState[] newStates = Constants.SwerveConstants.kinematics.toSwerveModuleStates(discreteSpeeds);
+    SwerveDriveKinematics.desaturateWheelSpeeds(newStates, Constants.SwerveConstants.maxMetersPerSecond);
+    setModuleStates(newStates);
+  }
+
+  public SwerveModuleState[] getStates() {
+    SwerveModuleState[] states = new SwerveModuleState[4];
+    states[0] = frontLeft.getState();
+    states[1] = frontRight.getState();
+    states[2] = backLeft.getState();
+    states[3] = backRight.getState();
+
+    return states;
+  }
+
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    Logger.recordOutput("Target States", desiredStates);
+
+    // makes it never go above 5 m/s
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, SwerveConstants.maxMetersPerSecond);
+    // Sets the speed and rotation of each module
+    frontLeft.setDesiredStates(desiredStates[0]);
+    frontRight.setDesiredStates(desiredStates[1]);
+    backLeft.setDesiredStates(desiredStates[2]);
+    backRight.setDesiredStates(desiredStates[3]);
+  }
+  
+  public void drive(double forward, double strafe, double rotation, boolean isFieldRelative) {
+
+    /**
+     * ChassisSpeeds object to represent the overall state of the robot
+     * ChassisSpeeds takes a forward and sideways linear value and a rotational
+     * value
+     * 
+     * speeds is set to field relative or default (robot relative) based on
+     * parameter
+     */
+
+    ChassisSpeeds speeds = isFieldRelative
+        ? ChassisSpeeds.fromFieldRelativeSpeeds(forward, strafe, rotation, getHeading())
+        : new ChassisSpeeds(forward, strafe, rotation);
+
+    speeds = ChassisSpeeds.discretize(speeds, 0.02);
+
+    // use kinematics (wheel placements) to convert overall robot state to array of
+    // individual module states
+    SwerveModuleState[] states = SwerveConstants.kinematics.toSwerveModuleStates(speeds);
+
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, SwerveConstants.maxMetersPerSecond);
+
+    setModuleStates(states);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    odometry.update(getHeading(), getModulePositions());
+
+    SmartDashboard.putNumber("NavX Angle", navX.getAngle());
+    
+    Logger.recordOutput("Real States", getStates());
+    Logger.recordOutput("Pose", getPose());
+  }
+}
